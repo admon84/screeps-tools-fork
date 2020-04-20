87c6d1693c4f610a801d7d85d651ddb65a3d3d8b
Admon West <admonwest84@gmail.com>
2020-04-20 14:37:42 -0600

    Changed code indentation to 4 spaces

--- rollup.config.js
+++ rollup.config.js
@@ -5,68 +5,68 @@ import resolve from "rollup-plugin-node-resolve"
 import typescript from "rollup-plugin-typescript2"
 
 export default {
-  input: './src/mount.ts',
-  output: {
-    file: './public/js/app.js',
-    format: 'iife',
-    globals: {
-      fs: false,
-      path: false,
-      crypto: false
-    }
-  },
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    typescript({tsconfig: './tsconfig.json'}),
-    resolve(),
-    commonjs({
-      namedExports: {
-        'node_modules/react-dom/index.js': [
-            'render',
-        ],
-        'node_modules/react/index.js': [
-            'Component',
-            'PropTypes',
-            'createElement',
-            'Children'
-        ],
-        'node_modules/jquery/dist/jquery.js': [
-          'ajax'
-        ],
-        'node_modules/react-if/lib/ReactIf.js': [
-          'If',
-          'Then',
-          'Else'
-        ],
-        'node_modules/prop-types/index.js': [
-          'object'
-        ],
-        'node_modules/react-form/dist/index.js': [
-          'Form',
-          'Text',
-          'Select',
-          'StyledSelect',
-          'StyledText'
-        ],
-        'node_modules/lodash/lodash.js': [
-          'filter'
-        ],
-        'node_modules/lz-string/libs/lz-string.js': [
-          'compressToEncodedURIComponent',
-          'decompressFromEncodedURIComponent'
-        ],
-        'node_modules/react-ga/dist/react-ga.js': [
-          'initialize',
-          'event',
-          'set',
-          'pageview'
-        ]
-      }
-    }),
-    less({
-      output: './public/css/style.css'
-    })
-  ]
+    input: './src/mount.ts',
+    output: {
+        file: './public/js/app.js',
+        format: 'iife',
+        globals: {
+            fs: false,
+            path: false,
+            crypto: false
+        }
+    },
+    plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        typescript({tsconfig: './tsconfig.json'}),
+        resolve(),
+        commonjs({
+            namedExports: {
+                'node_modules/react-dom/index.js': [
+                    'render',
+                ],
+                'node_modules/react/index.js': [
+                    'Component',
+                    'PropTypes',
+                    'createElement',
+                    'Children'
+                ],
+                'node_modules/jquery/dist/jquery.js': [
+                    'ajax'
+                ],
+                'node_modules/react-if/lib/ReactIf.js': [
+                    'If',
+                    'Then',
+                    'Else'
+                ],
+                'node_modules/prop-types/index.js': [
+                    'object'
+                ],
+                'node_modules/react-form/dist/index.js': [
+                    'Form',
+                    'Text',
+                    'Select',
+                    'StyledSelect',
+                    'StyledText'
+                ],
+                'node_modules/lodash/lodash.js': [
+                    'filter'
+                ],
+                'node_modules/lz-string/libs/lz-string.js': [
+                    'compressToEncodedURIComponent',
+                    'decompressFromEncodedURIComponent'
+                ],
+                'node_modules/react-ga/dist/react-ga.js': [
+                    'initialize',
+                    'event',
+                    'set',
+                    'pageview'
+                ]
+            }
+        }),
+        less({
+            output: './public/css/style.css'
+        })
+    ]
 }
